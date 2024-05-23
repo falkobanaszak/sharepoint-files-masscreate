f0063e14dd87445ef302379e7bb4214186275c0d
falkobanaszak <falko@banaszak.de>
2024-05-23 09:14:54 +0200

    added the script

--- sharepoint-files-masscreate.ps1
+++ sharepoint-files-masscreate.ps1
@@ -28,3 +28,50 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 #>
+# Import the SharePoint Online Management Shell Module
+# You can download it here: https://www.microsoft.com/en-us/download/details.aspx?id=35588
+# Or just use "Install-Module SharePointPnPPowerShellOnline -Scope CurrentUser"
+Import-Module SharePointPnPPowerShellOnline
+
+# Variables 
+$SharePointSiteURL = "https://m365x49111172.sharepoint.com/sites/sharepoint-files-masscreate"
+$SharePointLibraryName = "mass-files-library"
+$MassFileCount = 100000  # Number of files to create
+$MassFilePrefix = "RandomMassFile"  # Prefix for file names
+$MinimumFileSizeInKB = 1  # Minimum file size in KB
+$MaximumFileSizeInKB = 4096  # Maximum file size in KB
+
+# Connect to the SharePoint Online Site
+Connect-PnPOnline -Url $SharePointSiteURL -UseWebLogin
+
+# Function to create mass files in the SharePoint Library with random size
+function Create-MassFiles {
+    param (
+        [string]$RandomFileName,
+        [string]$LibraryName,
+        [int]$MinSizeKB,
+        [int]$MaxSizeKB
+    )
+    
+    # Generate random file size in KB
+    $random = New-Object System.Random
+    $fileSizeKB = $random.Next($MinSizeKB, $MaxSizeKB + 1)
+    
+    # Create the random content for the file
+    $fileContent = New-Object byte[] ($fileSizeKB * 1024)
+    $random.NextBytes($fileContent)
+    
+    # Upload files to the document library
+    $fileStream = New-Object IO.MemoryStream (,$fileContent)
+    Add-PnPFile -Folder $LibraryName -FileName $RandomFileName -Stream $fileStream
+    Write-Host "Created file: $FileName with size: $fileSizeKB KB"
+}
+
+# Create a loop for multiple files
+for ($i = 1; $i -le $MassFileCount; $i++) {
+    $fileName = "$MassFilePrefix$i.txt"
+    Create-MassFiles -FileName $fileName -LibraryName $SharePointLibraryName -MinSizeKB $MinimumFileSizeInKB -MaxSizeKB $MaximumFileSizeInKB
+}
+
+# Disconnect from SharePoint Online
+Disconnect-PnPOnline
